04ce28bd171735ea53d4bd5463f915479041fc52
Viney Chhillar <ronaessi28chhillar@gmail.com>
2025-08-07 23:47:16 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css'; // Assuming you might have some global CSS here
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+ 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
